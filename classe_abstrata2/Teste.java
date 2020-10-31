d0df6ad1b5fa168338ba78bf4d950e2cfcc6b996
Alephy Vinicius <45470656+AlephyV@users.noreply.github.com>
2020-10-31 15:07:05 -0300

    Update Teste.java

--- classe_abstrata2/Teste.java
+++ classe_abstrata2/Teste.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 public class Teste {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        ArrayList<Homem> homens = new ArrayList();
-        ArrayList<Mulher> mulheres = new ArrayList();
+        ArrayList<Pessoa> pessoas = new ArrayList();
         while(true){
             System.out.println("1. Cadastra Homem\n2. Cadastrar Mulher\n3. Consultar IMC (Busca pelo nome)\n4. Listar Homens\n5. Listar Mulheres\n6. Listar todos os IMCs dos Homens\n7. Listas todos os IMCs\n0. Sair");
             int escolha = teclado.nextInt();
@@ -20,7 +19,7 @@ public class Teste {
                     System.out.println("Digite seu peso: ");
                     double peso = teclado.nextDouble();
                     Homem homem = new Homem(altura, peso, nome, dataNascimento);
-                    homens.add(homem);
+                    pessoas.add(homem);
                 break;
                 case 2:
                     System.out.println("Digite seu nome: ");
@@ -32,50 +31,59 @@ public class Teste {
                     System.out.println("Digite seu peso: ");
                     peso = teclado.nextDouble();
                     Mulher mulher = new Mulher(altura, peso, nome, dataNascimento);
-                    mulheres.add(mulher);
+                    pessoas.add(mulher);
                 break;
                 case 3:
                     System.out.println("Nome para a consulta: ");
                     nome = teclado.next();
-                    for(int i = 0; i < homens.size(); i++){
-                        Homem h = homens.get(i);
-                        if(h.getNome().equals(nome)){
-                            System.out.println(h.resultIMC());
+                    for(int i = 0; i < pessoas.size(); i++){
+                        if(pessoas.get(i) instanceof Mulher) {
+                            Mulher m = (Mulher) pessoas.get(i);
+                            if(m.getNome().equals(nome)){
+                                System.out.println(m.resultIMC());
+                            }
+                        } else if(pessoas.get(i) instanceof Homem) {
+                            Homem h = (Homem) pessoas.get(i);
+                            if(h.getNome().equals(nome)){
+                                System.out.println(h.resultIMC());
+                            }
                         }
-                    }
-                    for(int i = 0; i < mulheres.size(); i++){
-                        Mulher m = mulheres.get(i);
-                        if(m.getNome().equals(nome)){
-                            System.out.println(m.resultIMC());
-                        }
+                        
                     }
                 break;
                 case 4:
-                    for(int i = 0; i < homens.size(); i++){
-                        Homem h = homens.get(i);
-                        System.out.println(h.getNome());
+                    for(int i = 0; i < pessoas.size(); i++){
+                        if(pessoas.get(i) instanceof Homem) {
+                            Homem h = (Homem) pessoas.get(i);
+                            System.out.println(h.getNome());
+                        } 
                     }
                 break;
                 case 5:
-                    for(int i = 0; i < mulheres.size(); i++){
-                        Mulher m = mulheres.get(i);
-                        System.out.println(m.getNome());
+                    for(int i = 0; i < pessoas.size(); i++){
+                        if(pessoas.get(i) instanceof Mulher) {
+                            Mulher m = (Mulher) pessoas.get(i);
+                            System.out.println(m.getNome());
+                        } 
                     }
                 break;
                 case 6:
-                    for(int i = 0; i < homens.size(); i++){
-                        Homem h = homens.get(i);
-                        System.out.println(h.getNome() + " - " + h.resultIMC());
+                    for(int i = 0; i < pessoas.size(); i++){
+                        if(pessoas.get(i) instanceof Homem) {
+                            Homem h = (Homem) pessoas.get(i);
+                            System.out.println(h.getNome() + " - " + h.resultIMC());
+                        } 
                     }
                 break;
                 case 7:
-                    for(int i = 0; i < homens.size(); i++){
-                        Homem h = homens.get(i);
-                        System.out.println(h.getNome() + " - " + h.resultIMC());
-                    }
-                    for(int i = 0; i < mulheres.size(); i++){
-                        Mulher m = mulheres.get(i);
-                        System.out.println(m.getNome() + " - " + m.resultIMC());
+                    for(int i = 0; i < pessoas.size(); i++){
+                        if(pessoas.get(i) instanceof Mulher) {
+                            Mulher m = (Mulher) pessoas.get(i);
+                            System.out.println(m.getNome() + " - " +m.resultIMC());
+                        } else if(pessoas.get(i) instanceof Homem) {
+                            Homem h = (Homem) pessoas.get(i);
+                            System.out.println(h.getNome() + " - " + h.resultIMC());
+                        }
                     }
                 break;
                 default:
